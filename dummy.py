5198b46829db841a04f3e2480c13f670741fdec5
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2021-08-21 10:15:56 +0100

    Add logging.

--- dummy.py
+++ dummy.py
@@ -1,5 +1,9 @@
-
 # A dummy package for testing out CI configurations
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 def hello():
+    logger.debug('Well, hello there.')
     return 'Hello, world!'
--- test_dummy.py
+++ test_dummy.py
@@ -1,12 +1,22 @@
-
 # A dummy test_harness for testing out CI configurations
+import logging
 import unittest
 
 from dummy import hello
 
+logger = logging.getLogger('test_dummy')
+
 class BaseTestCase(unittest.TestCase):
+    def setUp(self):
+        logger.debug('Set up for %s', self._testMethodName)
+
+    def tearDown(self):
+        logger.debug('Tear down for %s', self._testMethodName)
+
     def test_hello(self):
         self.assertEqual(hello(), 'Hello, world!')
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, filename='test_dummy.log',
+                        filemode='w', format='%(name)-10s %(message)s')
     unittest.main()
